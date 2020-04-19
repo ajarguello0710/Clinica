d5c3b76773e48f04d2e35709eb045db953470a47
Anggy Arguello <ajarguello0710@gmail.com>
2020-04-18 22:24:32 -0500

    Implementación de angular material y material module

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,16 +1,32 @@
+import { MaterialModule } from './_material/material/material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddressComponent } from './views/address/address.component';
+import { ConsultComponent } from './views/consult/consult.component';
+import { ConsultDetailComponent } from './views/consult-detail/consult-detail.component';
+import { DoctorComponent } from './views/doctor/doctor.component';
+import { PatientComponent } from './views/patient/patient.component';
+import { SpecialtyComponent } from './views/specialty/specialty.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddressComponent,
+    ConsultComponent,
+    ConsultDetailComponent,
+    DoctorComponent,
+    PatientComponent,
+    SpecialtyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
