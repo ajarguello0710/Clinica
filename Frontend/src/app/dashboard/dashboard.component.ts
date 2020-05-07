3db90ddfd2bea2ec7eda1ce6b9d44ccd6f0be684
Sebastian Roldan <sebasroldanm@gmail.com>
2020-05-07 09:40:41 -0500

    Ajuste de Plantilla
    Se intento buscar un error en Js Consola [Cannot read property
    'appendChild' of null] pero no encontre solución
    
    Cambio unos parametros de json e icons

--- Frontend/src/app/dashboard/dashboard.component.ts
+++ Frontend/src/app/dashboard/dashboard.component.ts
@@ -7,66 +7,68 @@ declare var require: any;
 const data: any = require('./data.json');
 
 export interface Chart {
-	type: ChartType;
-	data: Chartist.IChartistData;
-	options?: any;
-	responsiveOptions?: any;
-	events?: ChartEvent;
+  type: ChartType;
+  data: Chartist.IChartistData;
+  options?: any;
+  responsiveOptions?: any;
+  events?: ChartEvent;
 }
 
 @Component({
-	selector: 'app-dashboard',
-	templateUrl: './dashboard.component.html',
-	styleUrls: ['./dashboard.component.scss']
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements AfterViewInit {
-	ngAfterViewInit() {}
+  ngAfterViewInit() { }
 
-	// Barchart
-	barChart1: Chart = {
-		type: 'Bar',
-		data: data['Bar'],
-		options: {
-			seriesBarDistance: 15,
-			high: 12,
+  // Barchart
+  // tslint:disable-next-line: member-ordering
+  barChart1: Chart = {
+    type: 'Bar',
+    data: data['Bar'],
+    options: {
+      seriesBarDistance: 15,
+      high: 12,
 
-			axisX: {
-				showGrid: false,
-				offset: 20
-			},
-			axisY: {
-				showGrid: true,
-				offset: 40
-			},
-			height: 360
-		},
+      axisX: {
+        showGrid: false,
+        offset: 20
+      },
+      axisY: {
+        showGrid: true,
+        offset: 40
+      },
+      height: 360
+    },
 
-		responsiveOptions: [
-			[
-				'screen and (min-width: 640px)',
-				{
-					axisX: {
-						labelInterpolationFnc: function(
-							value: number,
-							index: number
-						): string {
-							return index % 1 === 0 ? `${value}` : null;
-						}
-					}
-				}
-			]
-		]
-	};
+    responsiveOptions: [
+      [
+        'screen and (min-width: 640px)',
+        {
+          axisX: {
+            labelInterpolationFnc: function (
+              value: number,
+              index: number
+            ): string {
+              return index % 1 === 0 ? `${value}` : null;
+            }
+          }
+        }
+      ]
+    ]
+  };
 
-	// This is for the donute chart
-	donuteChart1: Chart = {
-		type: 'Pie',
-		data: data['Pie'],
-		options: {
-			donut: true,
-			height: 260,
-			showLabel: false,
-			donutWidth: 20
-		}
-	};
+  // This is for the donute chart
+  // tslint:disable-next-line: member-ordering
+  donuteChart1: Chart = {
+    type: 'Pie',
+    data: data['Pie'],
+    options: {
+      donut: true,
+      height: 260,
+      showLabel: false,
+      donutWidth: 20
+    }
+  };
 }
