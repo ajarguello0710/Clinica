9b3e2565e4dc7b89bc7a29b3accce1087a338891
Sebastian Roldan <sebasroldanm@gmail.com>
2020-05-21 12:19:22 -0500

    CRUD Pacient y Doctor
    Falta el deshabiitar para todos

---
+++ Frontend/src/app/views/doctor/dialog-doctor/dialog-doctor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-doctor',
+  templateUrl: './dialog-doctor.component.html',
+  styleUrls: ['./dialog-doctor.component.css']
+})
+export class DialogDoctorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ Frontend/src/app/views/patient/dialog-patient/dialog-patient.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-patient',
+  templateUrl: './dialog-patient.component.html',
+  styleUrls: ['./dialog-patient.component.css']
+})
+export class DialogPatientComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
